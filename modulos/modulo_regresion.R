e9574cfff9f2a414d4651fac2fa879263e9280ee
dataideachile <juan@dataidea.org>
2021-11-13 21:29:22 +0000

    se actualiza regresion

--- modulos/modulo_regresion.R
+++ modulos/modulo_regresion.R
@@ -9,19 +9,46 @@ reg_ui <- function(id, label = "reg") {
   tabItem(tabName = "reg",
           sidebarLayout(
             sidebarPanel(selectInput(inputId = ns("selector1"),"Seleccion de set de datos",choices = c("iris"="iris","beaver1"="beaver1","mtcars"="mtcars","trees"="trees")),
-                         selectInput(inputId = ns("selector2"),"Seleccion de color",choices = c("viridis"="viridis","magma"="magma","cividis"="cividis","plasma"="plasma")),
+                         # selectInput(inputId = ns("selector2"),"Seleccion de color",choices = c("viridis"="viridis","magma"="magma","cividis"="cividis","plasma"="plasma")),
+                         
+                         
+                         radioGroupButtons(
+                           inputId = ns("selector2"),
+                           label = "Selección de color", 
+                           choices = c("viridis"="viridis","magma"="magma","cividis"="cividis","plasma"="plasma"),
+                           status = "primary"
+                         ),
+                         
+                         
+                         
                          sliderInput(inputId = ns("selector3"),"Tamano:",min=0,max=10,value=5),
                          sliderInput(inputId = ns("selector4"),"Alpha:",min=0,max=1,value=1,animate = TRUE),
                          sliderInput(inputId = ns("selector5"),"Shape:",min=0,max=25,value=16,animate = TRUE),
-                         checkboxInput(inputId = ns("lineal"), "Ajustar modelo de regresion lineal", value = FALSE),
+                         # checkboxInput(inputId = ns("lineal"), "Ajustar modelo de regresion lineal", value = FALSE),
+                         
+                         
+                         
+                         
+                         awesomeRadio(
+                           inputId = ns("reg"),
+                           label = "Radio buttons", 
+                           choices = c("Lineal"="lin", "No Lineal"="nolin"),
+                           selected = "lin",
+                           inline = TRUE, 
+                           status = "success"
+                         ),
+                         
+                         
+                         
+                         
                          checkboxInput(inputId = ns("r2"), "Mostar estadistico del modelo", value = FALSE),width=4),
-            mainPanel(box(plotOutput(ns("graf1")),width=12))),
+            mainPanel(box(plotOutput(ns("graf1")),width=12,collapsible = FALSE,elevation = 2,headerBorder = FALSE))),
           fluidRow(bs4InfoBox(title = "Estadisticas del eje x",value = htmlOutput(ns("stat_x")),color = "info",gradient = TRUE,
                               elevation = 3,fill = TRUE,width = 4),
                    
                    
                    bs4InfoBox(title = "Estadisticas del eje y",value = htmlOutput(ns("stat_y")),color="info",gradient = TRUE,
-                               elevation = 3,fill = TRUE,width = 4),
+                               elevation = ,fill = TRUE,width = 4),
                    bs4InfoBox(title = "Estadisticas del modelo",value = htmlOutput(ns("r2")),color = "primary",gradient = TRUE,
                               elevation = 3,fill = TRUE,width = 4)))
                    
@@ -32,11 +59,14 @@ reg_sv <- function(input, output, session) {
   base <- reactive({get(input$selector1)})
   # shape<-as.character(input$selector5)
   
+  observeEvent(input$reg, priority=1,{
   output$graf1 <- renderPlot({
+   
     
     
-    
-  if(input$lineal==TRUE) {
+      reg<-input$reg
+      
+  if(reg=="lin"){
   
     data <- base()
     # highchart() %>%
@@ -44,7 +74,8 @@ reg_sv <- function(input, output, session) {
     #   hc_add_series(data, "scatter", hcaes(x = data[,1], y = data[,2]), name = "Pie")
       ggplot(data,aes(x=data[,1],y=data[,2],color=data[,3]))+
         # geom_point(col="red",size=0.3,alpha=0.7)+geom_smooth(method = "lm")+
-        geom_point(size=input$selector3,alpha=input$selector4,shape=input$selector5)+geom_smooth(method = "lm")+
+        geom_point(size=input$selector3,alpha=input$selector4,shape=input$selector5)+
+        geom_smooth(method = "lm",formula=y~x)+
         xlab("Eje x")+ylab("Eje y") + 
         scale_color_viridis_c(option = input$selector2)
       
@@ -52,7 +83,20 @@ reg_sv <- function(input, output, session) {
       # p2 <- gb + scale_color_viridis_c(option = "inferno") + ggtitle("'inferno'")
       # p3 <- gb + scale_color_viridis_c(option = "plasma") + ggtitle("'plasma'")
       # p4 <- gb + scale_color_viridis_c(option = "cividis") + ggtitle("'cividis'")
+    }else if(reg=="nolin"){
       
+      data <- base()
+      # highchart() %>%
+      #   # Data
+      #   hc_add_series(data, "scatter", hcaes(x = data[,1], y = data[,2]), name = "Pie")
+      ggplot(data,aes(x=data[,1],y=data[,2],color=data[,3]))+
+        # geom_point(col="red",size=0.3,alpha=0.7)+geom_smooth(method = "lm")+
+        geom_point(size=input$selector3,alpha=input$selector4,shape=input$selector5)+
+        geom_smooth(method = "lm",formula=y~poly(x,2))+
+        xlab("Eje x")+ylab("Eje y") + 
+        scale_color_viridis_c(option = input$selector2)
+      
+       
     }else{
       data <- base()
       ggplot(data,aes(x=data[,1],y=data[,2],color=data[,3]))+
@@ -61,19 +105,35 @@ reg_sv <- function(input, output, session) {
         xlab("Eje x")+ylab("Eje y") + 
         scale_color_viridis_c(option=input$selector2)
     }
-
     })
-  
-  
-  output$r2 <- renderText({
-    if(input$r2==TRUE) {
+  })
+
+    observeEvent(input$reg, priority=1,{
+    
+    output$r2 <- renderText({
+    
+    reg<-input$reg
+        
+    if(input$r2==TRUE){
+      
+    if(reg=="lin"){
       
     mydata <- base()
     modelo<-lm(data = mydata,mydata[,1]~mydata[,2])
     paste0("R2: ",round(summary(modelo)$r.squared,2),br(),br(),
            "R2 ajustado x: ",round(summary(modelo)$adj.r.squared,2),br(),
            "Ajuste lineal: Y=",round(modelo$coefficients[1],2),"+",round(modelo$coefficients[2],2),"X",br())
+  
+    }else if(reg=="nolin"){
+      
+    mydata <- base()
+    modelo<-lm(data = mydata,mydata[,1]~poly(mydata[,2],2))
+    paste0("R2: ",round(summary(modelo)$r.squared,2),br(),br(),
+          "R2 ajustado x: ",round(summary(modelo)$adj.r.squared,2),br(),
+          "Ajuste lineal: Y=",round(modelo$coefficients[1],2),"+",round(modelo$coefficients[2],2),"X^2",br())
+    }
     }else{}    
+  })
   })
   
   
